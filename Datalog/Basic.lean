f2d89c98883ab076d184a6db5400ebb656b8bb2e
frakappa <francesco.cappetti+github@pm.me>
2026-05-18 22:23:04 +0200

    Custom syntax

--- Datalog/Basic.lean
+++ Datalog/Basic.lean
@@ -36,3 +36,44 @@ instance : ToString Program where
   .mk (.mk "ancestor" [.var "X", .var "Y"]) [.mk "parent" [.var "X", .var "Y"]],
   .mk (.mk "ancestor" [.var "X", .var "Y"]) [.mk "parent" [.var "X", .var "Z"], .mk "ancestor" [.var "Z", .var "Y"]],
 ] : Program)
+
+declare_syntax_cat datalog_term
+declare_syntax_cat datalog_atom
+declare_syntax_cat datalog_rule
+declare_syntax_cat datalog_program
+
+syntax ident : datalog_term
+syntax ident ("(" datalog_term,* ")")? : datalog_atom
+syntax datalog_atom (":-" datalog_atom,*)? "." : datalog_rule
+syntax datalog_rule* : datalog_program
+
+syntax "[Term|" datalog_term "]" : term
+syntax "[Atom|" datalog_atom "]" : term
+syntax "[Rule|" datalog_rule "]" : term
+syntax "[Program|" datalog_program "]" : term
+
+open Lean.Syntax (mkStrLit) in
+macro_rules
+  | `([Term| $s:ident]) =>
+    let s := s.getId.toString
+    if s.front.isUpper then
+      `(Term.var $(mkStrLit s))
+    else
+      `(Term.const $(mkStrLit s))
+  | `([Atom| $rel:ident]) =>
+    `(Atom.mk $(mkStrLit rel.getId.toString) [])
+  | `([Atom| $rel:ident ( $[$terms:datalog_term],* )]) =>
+    `(Atom.mk $(mkStrLit rel.getId.toString) [ $[[Term| $terms]],* ])
+  | `([Rule| $head:datalog_atom .]) =>
+    `(Rule.mk [Atom| $head] [])
+  | `([Rule| $head:datalog_atom :- $[$body:datalog_atom],* .]) =>
+    `(Rule.mk [Atom| $head] [ $[[Atom| $body]],* ])
+  | `([Program| $[$prog:datalog_rule]*]) =>
+    `([ $[[Rule| $prog]],* ])
+
+#check [Program|
+parent(xerces, brooke).
+parent(brooke, damocles).
+ancestor(X, Y) :- parent(X, Y).
+ancestor(X, Y) :- parent(X, Z), ancestor(Z, Y).
+]
