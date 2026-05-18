31baa2423e0c6f15b133b0c039bd4fc3867dbc9b
frakappa <francesco.cappetti+github@pm.me>
2026-05-18 16:18:59 +0200

    Basic data structures and string representation

--- Datalog/Basic.lean
+++ Datalog/Basic.lean
@@ -1,1 +1,38 @@
-def hello := "world"
+inductive Term where
+  | const : String → Term
+  | var : String → Term
+
+instance : ToString Term where
+  toString
+    | .const s | .var s => s
+
+structure Atom where
+  rel : String
+  terms : List Term
+
+instance : ToString Atom where
+  toString
+    | Atom.mk rel [] => rel
+    | Atom.mk rel terms => rel ++ "(" ++ ", ".intercalate (terms.map toString) ++ ")"
+
+structure Rule where
+  head : Atom
+  body : List Atom
+
+instance : ToString Rule where
+  toString
+    | Rule.mk head [] => toString head ++ "."
+    | Rule.mk head body => toString head ++ " :- " ++ ", ".intercalate (body.map toString) ++ "."
+
+abbrev Program := List Rule
+
+instance : ToString Program where
+  toString prog :=
+    "\n".intercalate (prog.map toString)
+
+#eval ([
+  .mk (.mk "parent" [.const "xerces", .const "brooke"]) [],
+  .mk (.mk "parent" [.const "brooke", .const "damocles"]) [],
+  .mk (.mk "ancestor" [.var "X", .var "Y"]) [.mk "parent" [.var "X", .var "Y"]],
+  .mk (.mk "ancestor" [.var "X", .var "Y"]) [.mk "parent" [.var "X", .var "Z"], .mk "ancestor" [.var "Z", .var "Y"]],
+] : Program)
